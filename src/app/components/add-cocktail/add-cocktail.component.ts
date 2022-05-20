14987964b0ab616feee7938d795467b9b421abf7
RossanaPetrar <rossana.petrar@gmail.com>
2022-05-20 20:05:35 +0300

    Added Tag Input UI and functionality in add-cocktail page - WIP.

--- src/app/components/add-cocktail/add-cocktail.component.ts
+++ src/app/components/add-cocktail/add-cocktail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../../Cocktail';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-cocktail',
@@ -7,13 +8,40 @@ import { Cocktail } from '../../Cocktail';
   styleUrls: ['./add-cocktail.component.css'],
 })
 export class AddCocktailComponent implements OnInit {
+  faXmark = faXmark;
   name!: string;
   author!: string;
   ingredients!: string[];
   description!: string;
   image!: string;
+  tags: string[] = [];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  addTag(e: any) {
+    if (e.key == 'Enter') {
+      let tag: string = e.target.value.replace(/\s+/g, ' ');
+      if (tag.length > 1 && !this.tags.includes(tag))
+        tag.split(',').forEach((tag) => {
+          this.tags.push(tag);
+          console.log(this.tags);
+        });
+    } else return;
+    e.target.value = '';
+  }
+
+  remove(tag: string) {
+    let index = this.tags.indexOf(tag);
+    this.tags = [...this.tags.slice(0, index), ...this.tags.slice(index + 1)];
+    console.log(this.tags);
+  }
+
+  removeAll() {
+    this.tags.length = 0;
+    const ingredientTags = document.querySelectorAll('.ingredient-tags');
+    ingredientTags.forEach((ingredientTags) => ingredientTags.remove());
+    console.log(this.tags);
+  }
 }
